d75c6492e9fe798af8a8eaf91c46b6700b282347
Michael Thornbrugh <mpthornbrugh@gmail.com>
2016-04-25 20:13:38 -0500

    Fixed all of the possible errors and fixed error with history keeping.

--- histViewer/main/main.js
+++ histViewer/main/main.js
@@ -85,7 +85,9 @@ angular.module('histViewer.main', ['ngRoute'])
 				$timeout(waitForRenderAndDoSomething); // Wait for all templates to be loaded
 			} else {
 				for (var i in totalTimelineEvents) {
-					removePersons(totalTimelineEvents[i]);
+					if (totalTimelineEvents.hasOwnProperty(i)) {
+						removePersons(totalTimelineEvents[i]);
+					}
 				}
 				if (totalTimelineEvents.length == 1) {
 					$scope.person = totalTimelineEvents[0][0].who;
@@ -160,8 +162,10 @@ angular.module('histViewer.main', ['ngRoute'])
 				"left": left
 			};
 
+			var evCirc = $(".eventCircle");
+
 			//the event circle is animated when the mouse is hover over it.
-			$(".eventCircle").hover(function(){
+			evCirc.hover(function(){
 				$(this).addClass('animated bounceIn');
 				$(this).css("background-color", "green");
 			}, function(){
@@ -169,7 +173,7 @@ angular.module('histViewer.main', ['ngRoute'])
 				$(this).css("background-color", "#ff3700");
 			});
 
-			$(".eventCircle").each(function(i, obj) {
+			evCirc.each(function(i, obj) {
 				var objPos = $(obj).position();
 				if (objPos.top == $scope.currentEventLocation.top) {
 					if (Math.abs(objPos.left - $scope.currentEventLocation.left) <= 15 ) {
@@ -263,13 +267,15 @@ angular.module('histViewer.main', ['ngRoute'])
 
 		//This function returns the lowest date out of all of the events.
 		function getMinDate(events) {
-			var minDate;
+			var minDate = -1;
 			for (var i in events) {
-				if (!minDate) {
-					minDate = moment(new Date(events[i].when));
-				}
-				else if (moment(new Date(events[i].when)) < minDate) {
-					minDate = moment(new Date(events[i].when));
+				if (events.hasOwnProperty(i)) {
+					if (minDate == -1) {
+						minDate = moment(new Date(events[i].when));
+					}
+					else if (moment(new Date(events[i].when)) < minDate) {
+						minDate = moment(new Date(events[i].when));
+					}
 				}
 			}
 			return minDate;
@@ -277,13 +283,15 @@ angular.module('histViewer.main', ['ngRoute'])
 
 		//This function returns the highest date out of all the events.
 		function getMaxDate(events) {
-			var maxDate;
+			var maxDate = -1;
 			for (var i in events) {
-				if (!maxDate) {
-					maxDate = moment(new Date(events[i].when));
-				}
-				else if (moment(new Date(events[i].when)) > maxDate) {
-					maxDate = moment(new Date(events[i].when));
+				if (events.hasOwnProperty(i)) {
+					if (maxDate == -1) {
+						maxDate = moment(new Date(events[i].when));
+					}
+					else if (moment(new Date(events[i].when)) > maxDate) {
+						maxDate = moment(new Date(events[i].when));
+					}
 				}
 			}
 			maxDate.add(1, 'years');
@@ -297,8 +305,10 @@ angular.module('histViewer.main', ['ngRoute'])
 			addPersons(name);
 
 			for (var i in totalTimelineEvents) {
-				if (totalTimelineEvents[i][0].who != name) {
-					newTotalEvents.push(totalTimelineEvents[i]);
+				if (totalTimelineEvents.hasOwnProperty(i)) {
+					if (totalTimelineEvents[i][0].who != name) {
+						newTotalEvents.push(totalTimelineEvents[i]);
+					}
 				}
 			}
 
@@ -333,7 +343,7 @@ angular.module('histViewer.main', ['ngRoute'])
 				"class": 'timelineImage'
 			});
 
-			div.on("click", function(e){
+			div.on("click", function(){
 				var curName = $(this).find('.timelineName').text();
 				removeFromTotalEvents(curName);
 				$scope.$apply();
@@ -343,7 +353,7 @@ angular.module('histViewer.main', ['ngRoute'])
 				"class": 'timelineImageMapIcon'
 			});
 
-			mapDiv.on("click", function(e) {
+			mapDiv.on("click", function() {
 				var curName = $(this).parent().find('.timelineName').text();
 				HistoryService.setTimelineHistory(totalTimelineEvents);
 				$location.path('/map/' + curName);
@@ -361,16 +371,11 @@ angular.module('histViewer.main', ['ngRoute'])
 
 			var faMap = '<i class="fa fa-globe"></i>';
 
-			if ($scope.person.url) {
-				div.append(actualImg);
-			}
-			else {
-				div.append(fa);
-			}
+			div.append(actualImg);
 			mapDiv.append(faMap);
 
 			var person;
-			if ($scope.person.url) {
+			if ($scope.person && $scope.person.url) {
 				person = '<p class="timelineName" style="margin-top:-14px">';
 			}
 			else {
@@ -405,12 +410,14 @@ angular.module('histViewer.main', ['ngRoute'])
 				arr[i] = 0;
 			}
 
-			for (var i in events) {
-				var eventYear = moment(new Date(events[i].when)).year();
-				arr[(eventYear - minYear)] += 1;
-				if (arr[(eventYear - minYear)] > 2) {
-					needsAdjustment = true;
-					break;
+			for (var j in events) {
+				if (events.hasOwnProperty(j)) {
+					var eventYear = moment(new Date(events[j].when)).year();
+					arr[(eventYear - minYear)] += 1;
+					if (arr[(eventYear - minYear)] > 2) {
+						needsAdjustment = true;
+						break;
+					}
 				}
 			}
 			if (needsAdjustment) {
@@ -444,17 +451,23 @@ angular.module('histViewer.main', ['ngRoute'])
 			var minDate = 9999;
 
 			for (var i in eventArrays) {
-				minDates.push(getMinDate(eventArrays[i]).year());
-			}
-
-			for (var i in minDates) {
-				if (minDate > minDates[i]) {
-					minDate = minDates[i];
+				if (eventArrays.hasOwnProperty(i)) {
+					minDates.push(getMinDate(eventArrays[i]).year());
 				}
 			}
 
-			for (var i in minDates) {
-				returnArr.push({"yearDiff": (minDates[i] - minDate)});
+			for (var j in minDates) {
+				if (minDates.hasOwnProperty(j)) {
+					if (minDate > minDates[j]) {
+						minDate = minDates[j];
+					}
+				}
+			}
+
+			for (var k in minDates) {
+				if (minDates.hasOwnProperty(k)) {
+					returnArr.push({"yearDiff": (minDates[k] - minDate)});
+				}
 			}
 
 			return returnArr;
@@ -466,7 +479,7 @@ angular.module('histViewer.main', ['ngRoute'])
 				var events = totalEvents[totalEvents.length - 1];
 				var cont = $("#timelineContainer");
 				var viewWidth = cont.width();
-				var viewHeight = cont.height();
+				//var viewHeight = cont.height();
 				var switchNum = (location ? location : 1);
 				var midlineHeight;
 
@@ -562,15 +575,15 @@ angular.module('histViewer.main', ['ngRoute'])
 				var lineWidth = blankAreaOnSideOfTimeline + (sectionsNeeded * 120);
 
 				DrawLine(blankAreaOnSideOfTimeline, midlineHeight, lineWidth, midlineHeight);
-				for (var i = 0; i <= sectionsNeeded; i++) {
-					DrawLine((blankAreaOnSideOfTimeline + (120 * i)), (midlineHeight - 20), (blankAreaOnSideOfTimeline + (120 * i)), (midlineHeight + 20));
-					drawText((blankAreaOnSideOfTimeline + (120 * i)), (midlineHeight + 28), "" + (minYear + (yearGap * i)) + "");
+				for (var secCount = 0; secCount <= sectionsNeeded; secCount++) {
+					DrawLine((blankAreaOnSideOfTimeline + (120 * secCount)), (midlineHeight - 20), (blankAreaOnSideOfTimeline + (120 * secCount)), (midlineHeight + 20));
+					drawText((blankAreaOnSideOfTimeline + (120 * secCount)), (midlineHeight + 28), "" + (minYear + (yearGap * secCount)) + "");
 				}
 
 				if (yearGap > 1) {
 					var tickSpacing = 120/yearGap;
-					for (var i = 0; i < sectionsNeeded; i++) {
-						var start = blankAreaOnSideOfTimeline + 120*i;
+					for (var k = 0; k < sectionsNeeded; k++) {
+						var start = blankAreaOnSideOfTimeline + 120*k;
 						for (var j = 1; j <= yearGap; j++) {
 							var x = start + (tickSpacing * j);
 							DrawLine(x, (midlineHeight - 10), x, (midlineHeight + 10), 1);
@@ -579,8 +592,10 @@ angular.module('histViewer.main', ['ngRoute'])
 				}
 
 				//Draw all of the events.
-				for (var i in events) {
-					drawEvent(events[i], yearGap, midlineHeight, minYear, maxYear, blankAreaOnSideOfTimeline);
+				for (var event in events) {
+					if (events.hasOwnProperty(event)) {
+						drawEvent(events[event], yearGap, midlineHeight, minYear, maxYear, blankAreaOnSideOfTimeline);
+					}
 				}
 			}
 			else if (totalEvents.length < 1) {
@@ -590,9 +605,11 @@ angular.module('histViewer.main', ['ngRoute'])
 				var values = calculateEventSeparations(totalEvents);
 
 				for (var i in values) {
-					var a = [];
-					a.push(totalEvents[i]);
-					createTimeline(a, parseInt(i)+1, totalEvents[i][0].who, (120 * Math.floor(values[i].yearDiff/2)), 2);
+					if (values.hasOwnProperty(i)) {
+						var a = [];
+						a.push(totalEvents[i]);
+						createTimeline(a, parseInt(i)+1, totalEvents[i][0].who, (120 * Math.floor(values[i].yearDiff/2)), 2);
+					}
 				}
 			}
 		}
@@ -602,13 +619,15 @@ angular.module('histViewer.main', ['ngRoute'])
 			var people = [];
 			var currentPerson = "";
 			for (var i in $scope.allItems) {
-				if (!currentPerson || $scope.allItems[i].who != currentPerson) {
-					currentPerson = $scope.allItems[i].who;
-					var addItem = {
-						"name":currentPerson,
-						"url":$scope.allItems[i].imgUrl
-					};
-					people.push(addItem);
+				if ($scope.allItems.hasOwnProperty(i)) {
+					if (!currentPerson || $scope.allItems[i].who != currentPerson) {
+						currentPerson = $scope.allItems[i].who;
+						var addItem = {
+							"name":currentPerson,
+							"url":$scope.allItems[i].imgUrl
+						};
+						people.push(addItem);
+					}
 				}
 			}
 			people = $.unique(people);
